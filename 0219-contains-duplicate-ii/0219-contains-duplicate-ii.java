47f2dee122d96fc721b87c1079e68101bd728c81
SivaHariRaj <sivahariraj2410457@ssn.edu.in>
2026-09-06 14:10:28 +0530

    Time: 30 ms (46.02%), Space: 124.7 MB (12.61%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -5,7 +5,7 @@ class Solution {
         {
             if(map.containsKey(nums[i]))
             {
-                   if(Math.abs((map.get(nums[i])-i))<=k)
+                   if (i - map.get(nums[i]) <= k)
                    {
                     return true;
                    }
